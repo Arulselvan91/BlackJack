c71cb30f6982f06229efcce676e14188b948e671
Arulselvan91 <32482901+Arulselvan91@users.noreply.github.com>
2017-10-05 09:59:12 +0530

    Add files via upload

---
+++ CardGameFramework/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace CardGameFramework
+{
+    //Base class for player
+    public class Player
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+    }
+
+}
